30899565f3bf57231ce8906a6cc5a539b4c54865
Julio Espinosa <julio.i.espinosa@gmail.com>
2020-06-02 22:42:57 -0500

    pues cewl

---
+++ src/main/java/AntForDummies/App.java
@@ -0,0 +1,10 @@
+package AntForDummies;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("no mames!!!");
+        System.out.println("niceeeeee");
+    }
+
+}
