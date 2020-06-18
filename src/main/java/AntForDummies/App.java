74fe9abbfa1233dda35b8ea8fb8ad635976dbcb0
Julio Espinosa <julio.i.espinosa@gmail.com>
2020-06-18 05:44:48 -0500

    Now I have a ref lib that is being used in the project

--- src/main/java/AntForDummies/App.java
+++ src/main/java/AntForDummies/App.java
@@ -1,10 +1,20 @@
 package AntForDummies;
 
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("no mames!!!");
-        System.out.println("niceeeeee");
+        System.out.println("This is a dummie project to demo how to write an Ant file");
+        System.out.println("In this project, I am referencing a dependency in the lib directory.");
+        System.out.println("I am using apache commons cli and created an option\n\n");
+
+        Options o = new Options();
+        o.addOption("p", "print", false, "Send print request to printer");
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("AntForDummies", o);
     }
 
 }
